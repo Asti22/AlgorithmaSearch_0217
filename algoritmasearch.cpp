b200bf687665d92dd36489ef6b3b31ff5f04461a
Asti22 <uun3461@gmail.com>
2024-04-04 16:00:40 +0700

    ctr akan diingkremenkan jika array dalam i sama dengan variabel yang ada di item

--- algoritmasearch.cpp
+++ algoritmasearch.cpp
@@ -39,6 +39,13 @@ void input(){
         ctr=0;
         i=0; //step 2
         while (i < n)// step 3
+        {
+            ctr++;
+            if (arr[i]== item)
+            {
+                cout <<"\n"<< item <<"found at position"<<(i+1) <<endl; //step 6
+             }
+        }
         
     }
  }

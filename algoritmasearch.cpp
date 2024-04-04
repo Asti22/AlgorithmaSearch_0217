39e19489d79061163f53026893496eba67cbfc4e
Asti22 <uun3461@gmail.com>
2024-04-04 15:15:17 +0700

    menampilkan kondisi untuk Masukkan jumlah elemen dalam array dari dalam variabel n

--- algoritmasearch.cpp
+++ algoritmasearch.cpp
@@ -4,3 +4,10 @@ using namespace std;
 int arr[20]; //array to be searched
 int n; //number of elements in the array
 int i; //index of array element
+
+void input(){
+    while (true) {
+        cout << "Enter the number of elemnt in the array:";
+        cin>>n;
+    }
+}
